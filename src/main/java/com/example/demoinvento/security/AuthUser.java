86a88db6615ccf7916e716dcdddbcfddae912237
gaji12345 <gajimax12345@gmail.com>
2025-01-04 12:07:10 +0530

    AuthUser done

---
+++ src/main/java/com/example/demoinvento/security/AuthUser.java
@@ -0,0 +1,54 @@
+package com.example.demoinvento.security;/*  gaajiCode
+    99
+    04/01/2025
+    */
+
+
+import com.example.demoinvento.enitity.User;
+import lombok.Builder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+public class AuthUser implements UserDetails {
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
