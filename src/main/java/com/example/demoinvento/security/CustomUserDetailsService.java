9705c5b6fde411a69edd8dec30e7defd48136cbf
gaji12345 <gajimax12345@gmail.com>
2025-01-04 12:08:22 +0530

    CustomUserDetailsService done

---
+++ src/main/java/com/example/demoinvento/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.example.demoinvento.security;/*  gaajiCode
+    99
+    04/01/2025
+    */
+
+
+import com.example.demoinvento.enitity.User;
+import com.example.demoinvento.exeption.NotFoundException;
+import com.example.demoinvento.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepo userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new NotFoundException("User Email Not Found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
