01d70cc0b7d88f28b40f15f4ef88727581719902
gaji12345 <gajimax12345@gmail.com>
2025-01-07 08:16:49 +0530

    SupplierAPI done

---
+++ src/main/java/com/example/demoinvento/api/SupplierAPI.java
@@ -0,0 +1,50 @@
+package com.example.demoinvento.api;/*  gaajiCode
+    99
+    07/01/2025
+    */
+
+import com.example.demoinvento.dtos.Response;
+import com.example.demoinvento.dtos.SupplierDTO;
+import com.example.demoinvento.service.SupplierService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v3/supplier")
+@RequiredArgsConstructor
+public class SupplierAPI {
+    private final SupplierService supplierService;
+
+    @PostMapping("/add")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> addSupplier(@RequestBody @Valid SupplierDTO supplierDTO) {
+        return ResponseEntity.ok(supplierService.addSupplier(supplierDTO));
+    }
+
+
+    @GetMapping("/all")
+    public ResponseEntity<Response> getAllSuppliers() {
+        return ResponseEntity.ok(supplierService.getAllSupplier());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> getSupplierById(@PathVariable Long id) {
+        return ResponseEntity.ok(supplierService.getSupplierById(id));
+    }
+
+    @PutMapping("/update/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> updateSupplier(@PathVariable Long id, @RequestBody @Valid SupplierDTO supplierDTO) {
+        return ResponseEntity.ok(supplierService.updateSupplier(id, supplierDTO));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> deleteSupplier(@PathVariable Long id) {
+        return ResponseEntity.ok(supplierService.deleteSupplier(id));
+    }
+
+}
